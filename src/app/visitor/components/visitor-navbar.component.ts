921949c6addc43d2ccb0f11e7b34c5918a07c84a
balaji hambeere <balaji.hambeere@hotmail.com>
2019-11-20 22:16:07 +0530

    fixed issues

--- src/app/visitor/components/visitor-navbar.component.ts
+++ src/app/visitor/components/visitor-navbar.component.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-visitor-navbar',
     templateUrl: './visitor-navbar.component.html'
 })
-export class VisitorNavbarComponent {
-
-    constructor(private router: Router) { }
-
-    redirect() {
-        this.router.navigate(['']);
-    }
-}
+export class VisitorNavbarComponent { }
